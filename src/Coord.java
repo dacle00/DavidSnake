4b04affd184889c381d06193c5925b79fb2ec029
Dacle00 <dacle00@gmail.com>
2013-10-17 09:31:37 -0400

    Snake Nav update
    Added Coord class. added logic for Corners/turns. Added a tail which
    doesn't work right.

---
+++ src/Coord.java
@@ -0,0 +1,72 @@
+public class Coord {
+
+	public int x;
+	public int y;
+	
+	public Coord()
+	{
+		x=0;
+		y=0;
+	}
+	
+	public Coord(int pX, int pY)
+	{
+		x = pX;
+		y = pY;
+	};
+
+	/////////////
+	//overrides//
+	/////////////
+	
+	public String toString()
+	{
+		return(x + "," + y);
+	}
+
+	
+	public boolean equals(Coord c)
+	{
+		return( x==c.x && y==c.y );
+	}
+	
+	
+	//////////////////////////
+	// OTHER COOL FUNCTIONS //
+	//////////////////////////
+	
+	public boolean isHigherThan(Coord c)
+	{
+		return( y<c.y);
+	}
+	public boolean islowerThan(Coord c)
+	{
+		return( y>c.y);
+	}
+	public boolean isLefterThan(Coord c)
+	{
+		return( x<c.x);
+	}
+	public boolean isRighterThan(Coord c)
+	{
+		return( x<c.x);
+	}
+
+	public boolean isDirectlyAbove(Coord c)
+	{
+		return( y<c.y && x==c.x );
+	}
+	public boolean isDirectlyBelow(Coord c)
+	{
+		return( y>c.y && x==c.x );
+	}
+	public boolean isDirectlyLeftOf(Coord c)
+	{
+		return( x<c.x && y==c.y );
+	}
+	public boolean isDirectlyRightOf(Coord c)
+	{
+		return( x>c.x && y==c.y );
+	}
+
+}
